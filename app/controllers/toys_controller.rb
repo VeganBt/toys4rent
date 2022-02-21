bad3db51ddd8233367e23a28e33f454d6b77dfac
Ofer Peled <ofer2406@gmail.com>
2022-02-21 15:12:24 +0100

    added controller actions and routes

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,2 +1,30 @@
 class ToysController < ApplicationController
+  before_action :set_toy, only: [:edit, :update]
+
+  def index
+    @toys = Toy.all
+  end
+
+  def new
+    @toy = Toy.new
+  end
+
+  def create
+    @toy = Toy.new(toy_params)
+    if @toy.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def toy_params
+    params.require(:toy).permit(:name, :description, :category, :user_id)
+  end
+
+  def set_toy
+    @toy = Toy.find(params[:id])
+  end
 end
