d329ab201fce6a6e754b4a1f1d4d3e7823b85b48
Ofer Peled <ofer2406@gmail.com>
2022-02-22 17:51:55 +0100

    corrected the routes and the edit form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :booking, except: [:new, :create]
+  resources :bookings, except: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
