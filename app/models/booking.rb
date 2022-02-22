992c4c825cf6c4e325d24ad090bbf230eee1bc0b
Ofer Peled <ofer2406@gmail.com>
2022-02-22 11:38:53 +0100

    added validations and asociations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,12 @@
+require "date"
+
 class Booking < ApplicationRecord
+  STATUSES = %w[accepted declined pending completed]
+
   belongs_to :user
   belongs_to :toy
+
+  validates :start_date, presence: true, comparison: { greater_than_or_equal_to: Date.today }
+  validates :end_date, presence: true, comparison: { greater_than_or_equal_to: :start_date }
+  validates :status, presence: true, inclusion: { in: STATUSES }
 end
