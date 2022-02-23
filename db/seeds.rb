3a4cba779b7ad083e8c39bb33ebd534280ea1a9c
Ofer Peled <ofer2406@gmail.com>
2022-02-23 12:53:34 +0100

    added pictures to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'faker'
+require 'open-uri'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -18,6 +19,8 @@ User.destroy_all
 dates = [Date.today, Date.tomorrow, Date.today + 2, Date.today + 3]
 end_dates = dates.map { |date| date + 4 }
 
+puts "Creating 3 users...\n"
+
 users = 3.times.each_with_object([]) do |index, arr|
   arr << User.create!(
     first_name: Faker::Name.first_name,
@@ -27,24 +30,37 @@ users = 3.times.each_with_object([]) do |index, arr|
     password: "123456",
     password_confirmation: "123456",
   )
+  puts "User ##{index + 1}"
+  puts "Email: user#{index + 1}@gmail.com"
+  puts "Password: 123456"
+  puts ""
 end
 
 # Create some toys and associate them with users
+print "Creating toys..."
 toys = []
 
-12.times do
+12.times do |index|
   toy = Toy.new(
     name: "#{Faker::Games::Pokemon.name} toy",
     description: Faker::Lorem.paragraph,
     category: Toy::CATEGORIES.sample,
   )
   toy.user = users.sample
+  file = URI.open('https://source.unsplash.com/300x300/?toy')
+  toy.photo.attach(
+    io: file,
+    filename: "#{index}.jpg",
+    content_type: "image/jpg"
+  )
   toy.save!
   toys << toy
+  print "."
 end
 
 # Create some bookings
-
+puts ""
+puts "Creating bookings..."
 15.times do
   booking = Booking.new(
     start_date: dates.sample,
@@ -54,3 +70,5 @@ end
   booking.user = (users - [booking.toy.user]).sample
   booking.save!
 end
+
+puts "Done! 🎊"
