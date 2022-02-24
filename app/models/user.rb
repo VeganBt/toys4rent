62eeb014528a57d9fa2f4b6f3278398e447b01cc
Ofer Peled <ofer2406@gmail.com>
2022-02-24 13:01:12 +0100

    added city and country to user automatically

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,16 @@ class User < ApplicationRecord
   # Geocoding
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  after_validation :set_city_and_country
+
+  private
+
+  def set_city_and_country
+    results = Geocoder.search(address)
+    if results.first
+      self.city = results.first.city
+      self.country = results.first.country
+    end
+  end
 end
