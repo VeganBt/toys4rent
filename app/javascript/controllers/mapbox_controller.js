74d32dac4cea016d994afa57b87f7cddff5abca0
Ofer Peled <ofer2406@gmail.com>
2022-02-24 11:32:38 +0100

    added map functionality and markers

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -13,8 +13,17 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10",
-      center: [52.3, 4.9],
+      center: [4.9, 52.3],
       zoom: 9
-    })
+    });
+    this.#addMarkersToMap();
+  }
+
+  #addMarkersToMap() {
+    this.MarkersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(this.map)
+    });
   }
 }
