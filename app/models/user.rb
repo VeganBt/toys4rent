147d062223595bc17fd0d4fcb97ee589ec199f73
Ofer Peled <ofer2406@gmail.com>
2022-02-24 11:56:34 +0100

    Revert "added basic geocoding for User"
    This reverts commit e0d6c37d6a419a8a5d05ed23c13b7cdc68ee06eb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :address, presence: true
-  has_many :bookings
-  has_many :toys
-
-  # Geocoding
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+        validates :first_name, presence: true
+        validates :last_name, presence: true
+        validates :address, presence: true
+        has_many :bookings
+        has_many :toys
 end
